6a75e2b9556d17c8f94e6f04d68a9fc9a2ec5664
Axurq <xurq5953@gmail.com>
2021-04-17 08:48:46 +0800

    Initial commit

---
+++ Encryption.h
@@ -0,0 +1,29 @@
+//
+// Created by Axurq on 2021/4/16.
+//
+
+#ifndef DES_ENCRYPTION_H
+#define DES_ENCRYPTION_H
+
+
+
+
+
+
+void Encrypt();
+
+void IP();
+
+void converseIP();
+
+void roundFunction();
+
+void keyExtension();
+
+
+//test
+bool compare(int a, int b, const unsigned char * backup);
+void toggle(int a);
+void printText();
+
+#endif //DES_ENCRYPTION_H
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Encryption.h"
+extern unsigned char text[8];
+int main() {
+    using namespace std;
+//    cout << compare(14, 9, text);
+//    int i = 9;
+//    toggle(i);
+//    printf("%#x",text[i/8]);
+    IP();
+  //  converseIP();
+    printText();
+    return 0;
+}
